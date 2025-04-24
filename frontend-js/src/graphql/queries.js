a52e596b1a30d8fe82fb37d5fd082e20c206aa0b
Soudabeh Noorollahi <s.nourollahii@gmail.com>
2025-04-24 22:10:36 +0200

    del graphql folder

--- frontend-js/src/graphql/queries.js
+++ frontend-js/src/graphql/queries.js
@@ -1,23 +1,23 @@
-import { gql } from "@apollo/client";
+// import { gql } from "@apollo/client";
 
-export const GET_FLASHCARDS = gql`
-  query {
-    getFlashcards {
-      id
-      verb
-      preposition
-      meaning
-      difficulty
-      status
-    }
-  }
-`;
+// export const GET_FLASHCARDS = gql`
+//   query {
+//     getFlashcards {
+//       id
+//       verb
+//       preposition
+//       meaning
+//       difficulty
+//       status
+//     }
+//   }
+// `;
 
-export const UPDATE_FLASHCARD_STATUS = gql`
-  mutation UpdateFlashcardStatus($id: ID!, $status: String!) {
-    updateFlashcardStatus(id: $id, status: $status) {
-      _id
-      status
-    }
-  }
-`;
+// export const UPDATE_FLASHCARD_STATUS = gql`
+//   mutation UpdateFlashcardStatus($id: ID!, $status: String!) {
+//     updateFlashcardStatus(id: $id, status: $status) {
+//       _id
+//       status
+//     }
+//   }
+// `;
